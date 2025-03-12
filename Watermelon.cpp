c24acc728c114118d0484d66a15f11dc9b28466f
Sowrav-Bhowmik <sowravofficial721@gmail.com>
2025-03-12 22:24:16 +0600

    Add files via upload

---
+++ Watermelon.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+
+    cin >> num;
+
+    if(num%2==0 && num>2){
+       cout << "Yes\n";
+    }
+
+    else
+     cout << "NO\n";
+}
