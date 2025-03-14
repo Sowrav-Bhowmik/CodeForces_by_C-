3b2a9a6c0b701275e55403675c21aaca3837a57c
Sowrav Bhowmik <sowravofficial721@gmail.com>
2025-03-14 19:34:36 +0600

    Add files via upload

---
+++ Domino_piling.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int m, n, max;
+
+    cin >> m >> n;
+
+    max=(m*n)/2;
+
+    cout << max << endl;
+
+    return 0;
+}
